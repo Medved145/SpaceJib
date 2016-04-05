24ddefe2dcb54bb68f63937d6bd2d08c0a1befc2
Medved145 <bacardiparkhomencko@mail.ru>
2016-04-05 23:58:33 +0600

    Add Movement Ship

--- SpaceJib/Assets/Scripts/Player.cs
+++ SpaceJib/Assets/Scripts/Player.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-
+    
     public GameObject PlayerShip;
-    Vector2 shipPosition;
+    float inputX;
+    float inputY;
+
 	// Use this for initialization
 	void Start () {
         
@@ -12,6 +14,12 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
-	}
+        inputX += Input.GetAxis("Mouse X");
+        inputY += Input.GetAxis("Mouse Y");
+        Vector3 Position = new Vector3(inputX, inputY, 0);
+        //Vector3 shipPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        //transform.position += shipPosition * Time.deltaTime;
+        PlayerShip.transform.position = Position;//3.5f;
+        //Debug.Log(Input.mousePosition);
+    }
 }
