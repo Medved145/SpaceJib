34c11ef3d51e022f2835627c587f51055cf98984
Medved145 <bacardiparkhomencko@mail.ru>
2016-04-12 21:05:24 +0600

    Minor changes
    Add shoot on push mouse button;
    Add collisions to objects;

--- SpaceJib/Assets/Scripts/ShipShoot.cs
+++ SpaceJib/Assets/Scripts/ShipShoot.cs
@@ -5,6 +5,9 @@ public class ShipShoot : MonoBehaviour {
 
     public GameObject Shot;
     public GameObject PlayerShip;
+    private bool IsShoot = false;
+    public float ShootCooldawn = 0.1f;
+    private float SC = 0f;
 	// Use this for initialization
 	void Start () {
 	
@@ -14,10 +17,16 @@ public class ShipShoot : MonoBehaviour {
 	void Update () {
         Vector2 PosRes = PlayerShip.transform.position;
         PosRes.y = PosRes.y + 0.15f;
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0) && IsShoot == false)
         {
             //Debug.Log("Ship Shoot");
             GameObject shot = Instantiate(Shot, PosRes, Quaternion.identity) as GameObject;
+            IsShoot = true;
+            SC = ShootCooldawn;
         }
+        if (SC > 0)
+            SC -= Time.deltaTime;
+        if (SC <= 0)
+            IsShoot = false;
 	}
 }
